6e4f0657586265a4b1aa87d9c26159928259c007
Rinor <rinorjahaj392@gmail.com>
2021-02-04 12:02:51 +0100

    Project Initialization with PHP

---
+++ includes/navbar.php
@@ -0,0 +1,20 @@
+<div class="menu-btn">
+        <i class="fas fa-bars fa-2x"></i>
+    </div>
+
+    <div class="navbar">
+        <div class="container flex">
+            <a href='index.php'>
+            <i class="fas fa-globe fa-2x webIcon"></i>
+            </a>
+            <nav class="main-nav">
+                <ul class="main-menu">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="aboutUs.php">About Us</a></li>
+                    <li><a href="event.php">Create Event</a></li>
+                    <li><a href="index.php">Events</a></li>
+                    <li><a href="login.php"><i class="fas fa-user"></i></a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
