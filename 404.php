dc0f3aa65d7f75decaef2f02c034d3989c3717bc
Shpetimbuqa <60103646+Shpetimbuqa@users.noreply.github.com>
2021-03-12 18:57:18 +0100

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php include 'includes/navbar.php'; ?>
+<style>
+.notfound {}
+
+.notfound h2 {
+    font-size: 80px;
+    line-height: 130px;
+    text-align: center;
+}
+</style>
+
+
+<div class="main">
+    <div class="content">
+        <div class="section group">
+            <div class="notfound">
+                <h2>Not found!</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
